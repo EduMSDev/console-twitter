139cd9ea6ee73fba38a33b981f202f5757e1a8ed
edu <edu@>
2022-10-14 20:50:52 +0200

    Incluida Excepcion

--- src/main/java/command/TwitterReceiver.java
+++ src/main/java/command/TwitterReceiver.java
@@ -1,5 +1,6 @@
 package command;
 
+import exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import model.Tweet;
 import model.User;
@@ -8,8 +9,6 @@ import singleton.ScannerSingleton;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -18,8 +17,8 @@ public class TwitterReceiver {
     private final ArrayList<User> users = new ArrayList<>();
     private User userLogged;
 
-    private Optional<User> findUser(String namePerson, List<User> listUser) {
-        return listUser.stream().filter(user -> user.getName().equalsIgnoreCase(namePerson)).findFirst();
+    private User findUser(String namePerson) {
+        return users.stream().filter(user -> user.getName().equalsIgnoreCase(namePerson)).findFirst().orElseThrow(UserNotFoundException::new);
     }
 
     private void calculateTime(Tweet tweet) {
@@ -40,11 +39,10 @@ public class TwitterReceiver {
             System.out.printf("%s enter the name of the user you want to change:: ", userLogged.getName());
             users.forEach(user -> System.out.printf(user.getName()));
             String name = ScannerSingleton.getInstance().next();
-            Optional<User> user = findUser(name, users);
-            if (user.isPresent()) {
-                userLogged = user.get();
-                System.out.printf("Welcome %s %n", user.get().getName());
-            } else {
+            try {
+                userLogged = findUser(name);
+                System.out.printf("Welcome %s %n", userLogged.getName());
+            } catch (UserNotFoundException e) {
                 System.err.printf("The user %s does not exist %n", name);
             }
         } else {
@@ -55,9 +53,9 @@ public class TwitterReceiver {
     public void createUser() {
         System.out.printf("%s  type the name of the user you want to include: ", userLogged.getName());
         String name = ScannerSingleton.getInstance().nextLine();
-        Optional<User> user = findUser(name, users);
+        boolean userExist = users.stream().noneMatch(user -> user.getName().equalsIgnoreCase(name));
         User userToAdd = null;
-        if (user.isEmpty()) {
+        if (userExist) {
             userToAdd = new User(name);
             users.add(userToAdd);
         }
@@ -71,17 +69,17 @@ public class TwitterReceiver {
     public void followUser() {
         System.out.printf("%s  enter the name of the user you want to follow: ", userLogged.getName());
         String namePerson = ScannerSingleton.getInstance().nextLine();
-        Optional<User> userExists = findUser(namePerson, users);
-        if (userExists.isPresent()) {
-            userLogged.addFriendToList(userExists.get());
-            String followed = String.format("%s ha seguido a %s", userLogged.getName(), userExists.get().getName());
+        try {
+            User userExists = findUser(namePerson);
+            userLogged.addFriendToList(userExists);
+            String followed = String.format("%s follows %s %n", userLogged.getName(), userExists.getName());
             Tweet tweet = Tweet.builder().message(followed).time(new Date().getTime()).build();
             userLogged.addTweet(tweet);
-            System.out.printf("%s follows  %s %n", userLogged.getName(), namePerson);
-        } else {
-            System.err.printf("User not found", namePerson);
+            System.out.printf(followed);
+        } catch (UserNotFoundException e) {
+            System.err.printf("User %s not found", namePerson);
         }
-        boolean userAdded = userExists.isPresent();
+        boolean userAdded = users.stream().anyMatch(user -> user.getName().equalsIgnoreCase(namePerson));
         String message = userAdded ? "User %s added %n" : "The user with name %s already exists %n";
         System.out.printf(message, namePerson);
     }
@@ -103,6 +101,7 @@ public class TwitterReceiver {
         userLogged.addTweet(tweet);
         System.out.println("Tweet posted!");
     }
+
     public void read() {
         System.out.printf("%s this is your personal wall: ", userLogged.getName());
         if (!userLogged.getTweets().isEmpty()) {
@@ -116,14 +115,14 @@ public class TwitterReceiver {
     public void unfollow() {
         System.out.printf("%s write the username: ", userLogged.getName());
         String name = ScannerSingleton.getInstance().next();
-        Optional<User> friendToRemove = this.findUser(name, userLogged.getFriends());
-        friendToRemove.ifPresent(userLogged::removeFriend);
-        if (friendToRemove.isPresent()) {
+        try {
+            User friendToRemove = this.findUser(name);
+            userLogged.removeFriend(friendToRemove);
             String tweet = String.format("%s has unfollowed %s %n", userLogged.getName(), name);
             Tweet unfollowedUser = Tweet.builder().message(tweet).time(new Date().getTime()).build();
             userLogged.addTweet(unfollowedUser);
             System.out.printf(tweet);
-        } else {
+        } catch (UserNotFoundException e) {
             System.err.printf("%s is not in your friends list  %n", name);
         }
     }
---
+++ src/main/java/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
